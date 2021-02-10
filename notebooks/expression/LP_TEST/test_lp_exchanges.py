9ab22d87833dca57d70a49cacc9ce03ab37039d8
hmbaghdassarian <hmbaghdassarian@eng.ucsd.edu>
2021-02-10 00:19:48 +0000

    change compartment of ECM transporters, works with cmp/gmp sinks

--- notebooks/expression/LP_TEST/test_lp_exchanges.py
+++ notebooks/expression/LP_TEST/test_lp_exchanges.py
@@ -22,14 +22,14 @@ from utils import functions as func
 from utils import parameters as params
 from utils.parameters import human_model as m_model
 from expression.build_me_model import flatten_list
-
-
-mu_val = 1e-9
-n_cores = 2
-base = 0
-counter = 3
+from macromolecules.macromolecule import Macromolecule
 
 lp_path = '/data2/hratch/human_me/other/test_lp/'
+mu_val = 1e-9
+n_cores = 2
+# base = 0
+counter = 5
+
 
 test_metab = list()
 for r in m_model.exchanges:
@@ -53,28 +53,19 @@ def add_sink(m, tme_new = None):
     tme_new.add_boundary(tme_new.metabolites.get_by_id(m_id), type ='sink')
     sln, stat, _ = tme_new.solve_lp(mu_val = mu_val)
     
-    if not os.path.isfile('test_sinks.tab'):
-        with open('test_sinks.tab', 'a+') as f:
+    if not os.path.isfile('test_sinks2.tab'):
+        with open('test_sinks2.tab', 'a+') as f:
             f.write('metabolite_id' + '\t' + 'status' + '\n')
         
         
-    with open('test_sinks.tab', 'a+') as f:
+    with open('test_sinks2.tab', 'a+') as f:
         f.write(m_id + '\t' + str(stat.max()) + '\n')
         
 
 with open(lp_path + 'working_version_' + str(counter) + '.pickle', 'rb') as handle:
     tme_new = pickle.load(handle)
 
-metabs_ = ['lys_L_e', 'gln_L_e', 'glu_L_l', 'asp_L_c', 'glu_L_c', 'lys_L_c', 'arg_L_c', 'asp_L_l', 'adp_c', 
-           'pi_c', 'glc_D_e', 'glc_D_c', 'adp_l', 'gsn_l', 'arg_L_l', 'atp_c', 'atp_l', 'gln_L_c', 'pi_l', 
-           'gsn_c'] 
-adp_c = tme_new.metabolites.get_by_id('adp_c')
-test = flatten_list([[m.id for m in r.products] for r in tme_new.reactions if adp_c in r.reactants])
-metabs_ = sorted(set(test).difference(metabs_))
-
-dadp_c = tme_new.metabolites.get_by_id('dadp_c')
-test = flatten_list([[m.id for m in r.products] for r in tme_new.reactions if adp_c in r.reactants])
-test = sorted(set(test).difference(metabs_))
+test = ['gmp_c']
 
 import multiprocessing
 import gc
